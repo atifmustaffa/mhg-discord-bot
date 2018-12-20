c31e91ed7d4b8a2938f7eafa92a17a671e698af9
Glitch (hello-express) <none>
2018-12-20 12:15:14 +0000

    🐑🏆 Checkpoint ./package.json:2594250/1355 ./server.js:2594250/1095 ./config.json:2594250/275 ./bots.js:2594250/73

---
+++ bots.js
@@ -0,0 +1,5 @@
+module.export = {
+  startBot: function() {
+    
+  }
+}
--- server.js
+++ server.js
@@ -20,3 +20,32 @@ app.get('/', function(request, response) {
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+const config = require("./config.json");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("message", (message) => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case 'hi':
+            message.channel.send(`Hey ${message.author}! Nok mintok tulong gapo deh hh?`);
+            break
+        case 'ping':
+            message.reply("pong!");
+            break
+        case 'dotabuff':
+            if (args[0] === 'match')
+                message.reply(`Check out your match here ${config.dotabuff}matches/${args[1]}`)
+            else if (args[0] === 'player')
+                message.reply(`Check out player history here ${config.dotabuff}players/${args[1]}`)
+            else
+                message.reply(`Invalid command bro. Please insert type and id eg: !dotabuff player 100846798`)
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
