e915afcae62f811fd2a9e8a6a02edd2e2dd1fedf
Glitch (hello-express) <none>
2018-12-20 12:26:13 +0000

    🎗📖 Checkpoint ./bots.js:2594250/2283 ./server.js:2594250/2142 ./package.json:2594250/84

--- server.js
+++ server.js
@@ -5,6 +5,9 @@
 const express = require('express');
 const app = express();
 
+const http = require('http');
+const bot = require('./bots.js');
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -14,6 +17,8 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
 });
 
 // listen for requests :)
@@ -21,31 +26,12 @@ const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-const config = require("./config.json");
-const Discord = require("discord.js");
-const client = new Discord.Client();
 
-client.on("message", (message) => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+// Code to keep the web alive, pinging itself
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
-    var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
 
-    switch (command) {
-        case 'hi':
-            message.channel.send(`Hey ${message.author}! Nok mintok tulong gapo deh hh?`);
-            break
-        case 'ping':
-            message.reply("pong!");
-            break
-        case 'dotabuff':
-            if (args[0] === 'match')
-                message.reply(`Check out your match here ${config.dotabuff}matches/${args[1]}`)
-            else if (args[0] === 'player')
-                message.reply(`Check out player history here ${config.dotabuff}players/${args[1]}`)
-            else
-                message.reply(`Invalid command bro. Please insert type and id eg: !dotabuff player 100846798`)
-    }
-});
-
-client.login(process.env.BOT_TOKEN);
+// startBot
+bot.startBot();
