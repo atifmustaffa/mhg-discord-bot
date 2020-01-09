3f361c94ff6234c872b237d3663613b458145289
Glitch (discord-bot-mhg) <none>
2020-01-09 17:08:47 +0000

    🚞🎅 Checkpoint ./potusScraper.js:2594250/8705

--- potusScraper.js
+++ potusScraper.js
@@ -16,42 +16,28 @@ async function getHTMLOutput() {
         let count = 0;
         let column = {}
         for (let j = 0; j < rows[i].children.length; j++) {
-          if (rows[i].children[j].name == "th") {
+          if (rows[i].children[j].name == "th" && rows[i].children[j].children.length == 1) {
             header.push(rows[i].children[j].children[0].data.trim().replace(/\s+/g, '_').toLowerCase())
           }
-          else if (rows[i].children[j].name == "td") {
+          else if (rows[i].children[j].name == "td" && rows[i].children[j].children.length == 1) {
             column[header[count]] = rows[i].children[j].children[0].data.trim();
             count++;
+          }          
+          else if (rows[i].children[j].name == "td" && rows[i].children[j].children.length > 1) {
+            column[header[count]] = rows[i].children[j].children[0].children[0].children[0].data.trim();
+            count++;
+            count = count/0;
           }
         }
         if (i > 0) leagues.push(column) // Exclude header
       }
-//       for (var i = 1; i < rows.length; i++) {
-//         console.log(rows[i].childNodes.length)
-//         var childNodes = rows[i].childNodes;
-//         var columns = [];
-
-//         for (var j = 0; j < childNodes.length; j++) {
-//           if (childNodes[j].tagName == "TD") {
-//             columns.push(childNodes[j]);
-//           }
-//         }
-
-//         console.log(columns.length);
-//         leagues.push({
-//           "title": columns[1].innerHTML,
-//           "date": columns[0].innerHTML,
-//           "dpc_points": columns[2].innerHTML
-//         });
-//       }
-      // leagues.push(rows[1]);
       // htmlOutput = {"noo": "test"};
       htmlOutput = leagues;
       // console.log(htmlOutput)
     })
     .catch(function(err) {
       //handle error
-      console.log(err)
+      console.warn(err)
     });
   return htmlOutput;
 }
