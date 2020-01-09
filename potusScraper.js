ce3a27d6eaf64a30f3e7d0cff5f2f733cd8f6a60
Glitch (discord-bot-mhg) <none>
2020-01-09 17:32:45 +0000

    🎁🛰 Checkpoint ./potusScraper.js:2594250/1519

--- potusScraper.js
+++ potusScraper.js
@@ -12,25 +12,31 @@ async function getHTMLOutput() {
       var rows = $("#mw-content-text > div > table.wikitable tr", html);
       // console.log(rows)
       const header = []
-      for (let i = 0; i < rows.length; i++) {
+      
+      // find last text element
+      let lastChild = function(el) {
+        if (el.children == undefined) return el;
+        else lastChild(el.children[0])
+      }
+            
+      for (let i = 0; i < rows.length-11; i++) {
         let count = 0;
         let column = {}
         for (let j = 0; j < rows[i].children.length; j++) {
           if (rows[i].children[j].name == "th") {
             header.push(rows[i].children[j].children[0].data.trim().replace(/\s+/g, '_').toLowerCase())
           }
+          else if (rows[i].children[j].name == "td") {
+            if (rows[i].children[j].children[0].name == "b")
+            // console.log("child->", rows[i].children[j].children[0].children[0].children[0])
+              console.log(lastChild(rows[i].children[j]))
+            // column[header[count]] = lastChild(rows[i].children[j]).data.trim();
+            // count++;
+          }
           // else if (rows[i].children[j].name == "td" && rows[i].children[j].children[0]) {
           //   column[header[count]] = rows[i].children[j].children[0].data.trim();
           //   count++;
           // }          
-          else if (rows[i].children[j].name == "td") {
-            let lastChild = function(el) {
-              if (!el.children) return el;
-              else lastChild(el.children[0])
-            }
-            column[header[count]] = lastChild(rows[i].children[j]).data.trim();
-            count++;
-          }
         }
         if (i > 0) leagues.push(column) // Exclude header
       }
