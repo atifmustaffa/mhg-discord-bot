ef5e1e6d34be4117f9292b3443f78bd597a36353
Atif Mustaffa <atifmustaffa@gmail.com>
2020-11-03 20:48:23 +0800

    update favicon

--- bots.js
+++ bots.js
@@ -39,7 +39,7 @@ function startBot() {
                     "description": "Commands List \n",
                     "color": parseInt(config.color.orange),
                     "thumbnail": {
-                        "url": "https://gamepedia.cursecdn.com/dota2_gamepedia/thumb/4/46/SeasonalRankTop4.png/140px-SeasonalRankTop4.png?version=c50e23ba43564fe0b153f569d84fab0d"
+                        "url": "https://media.discordapp.net/attachments/660818037292924938/714796385781940234/mhg-250x150.png"
                     },
                     "timestamp": new Date(2019, 8 - 1, 10, 11 - 8, 51, 0), // (year, month - 1, day, hour - 8, minute, second)
                     "footer": {
