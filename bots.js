49fb941ba55737fa9874bed0f779ba8adacea765
Atif Mustaffa <atifmustaffa@gmail.com>
2021-03-02 13:24:32 +0800

    fix: Fix untitled meme

--- bots.js
+++ bots.js
@@ -352,7 +352,7 @@ function startBot() {
                     if (args.length && args[args.length-1].substr(0, 4) == "http") {
                         let title = args.slice(0, -1).join(' ')
                         title = title.charAt(0).toLocaleUpperCase() + title.slice(1)
-                        message.channel.send(`> ${title}`, { files: [args[args.length-1]] })
+                        message.channel.send(title ? `> ${title}` : '', { files: [args[args.length-1]] })
                     } else {
                         message.channel.send(`Invalid arguments`).then(msg => msg.delete(2000))
                     }
