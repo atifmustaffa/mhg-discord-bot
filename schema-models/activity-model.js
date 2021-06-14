8f3d55bc9e3eb0656d5a7600eb79de9018c4e315
Atif Mustaffa <atifmustaffa@gmail.com>
2021-06-15 07:12:49 +0800

    feat: Add activity db model

---
+++ schema-models/activity-model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    id: String,
+    type: Number,
+    name: String
+})
+const model = mongoose.model('Activity', schema)
+
+module.exports = {
+    schema,
+    model
+}
+
+function getActivity(id) {
+    return model.findOne({ id })
+}
+
+function addActivity(newActivity) {
+    let activity = new model(newActivity)
+    return activity.save()
+}
+
+async function setActivity(newValue) {
+    // Function to add or set existing Activity
+    let activity = await getActivity(newValue.id)
+
+    if (activity) {
+        return model.updateOne({ id: newValue.id }, newValue, { upsert: true, new: true, setDefaultsOnInsert: true, useFindAndModify: false })
+    }
+    else {
+        return addActivity(newValue)
+    }
+}
+
+function deleteActivity(id) {
+    return model.deleteOne({ id })
+}
+
+module.exports = {
+    schema,
+    model,
+    getActivity,
+    addActivity,
+    setActivity,
+    deleteActivity,
+}
