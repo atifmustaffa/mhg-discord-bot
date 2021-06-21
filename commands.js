92d551ea9df8fa479ab85a8c3c93619c8c12a10f
Atif Mustaffa <atifmustaffa@gmail.com>
2021-06-21 13:05:34 +0800

    feat: Add dm handler

--- commands.js
+++ commands.js
@@ -1,4 +1,5 @@
 const config = require('./config.json')
+const helper =  require('./helper')
 
 const commands = {
     custommeme: require('./commands/custommeme'),
@@ -18,16 +19,26 @@ const commands = {
 module.exports = {
     commands: commands,
     handler: async(message) => {
-        // Check to avoid if message is not a command and not message from bot
+        // Handle direct message (guild null means dm)
+        if (!message.author.bot && !message.guild && commands['dm']) {
+            // Use existing dm command to forward msg to admin
+            commands['dm'].handler(
+                message, [
+                    config.adminId, `_Forwarded from_${helper.convertToSnowflake(message.author.id)}\n${message.content}`
+                ]
+            )
+        }
+
+        // Check to avoid if message is not a command or not a message from bot
         if (!message.content.startsWith(config.prefix) || message.author.bot) return
         // Convert message contents to args
         let args = message.content.slice(config.prefix.length).trim().split(/ +/)
         let command = args.shift().toLowerCase()
 
-        // Perform bot command
         if (commands[command]) {
             // Exit if admin command but user is not an admin
             if (commands[command].admin === true && message.author.id !== config.adminId) return
+            // Perform bot command
             commands[command].handler(message, command === 'help' ? commands : args)
         }
     }
