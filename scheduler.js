6e1be7d7b1f1ae3d6b47280e61dc45dc1f456cc0
Atif Mustaffa <atifmustaffa@gmail.com>
2021-06-21 23:12:00 +0800

    feat: Reschedule reminder stored in db and cleanup expired reminders

--- scheduler.js
+++ scheduler.js
@@ -1,4 +1,5 @@
 const schedule = require('node-schedule')
+const ReminderDB = require('./schema-models/reminder-model')
 
 module.exports = async(bot) => {
     // Execute all scheduled-job modules from files
@@ -11,4 +12,24 @@ module.exports = async(bot) => {
             job.handler(bot)
         })
     })
+
+    // Execute all schedule-jobs from database
+    // Find all reminders that remind_time is not passed yet
+    // Then reschedule reminder
+    let reminders = await ReminderDB.model.find({ remind_time: { $gt: new Date() }})
+    reminders.forEach((reminder) => {
+        schedule.scheduleJob(reminder.remind_time, () => {
+            bot.fetchUser(reminder.user_id, false).then((user) => {
+                user.send('Reminder: ' + reminder.notes)
+            })
+        })
+    })
+    // Delete expired reminder entry/document from db - cleanup
+    ReminderDB.model.deleteMany({ remind_time: { $lte: new Date() }})
+        .then((expired_reminders) => {
+            if (expired_reminders.length) console.info('Cleaned ' + expired_reminders.length + ' expired reminder(s)')
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
 }
